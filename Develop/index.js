2765815e2123176f094b0ac205735e38f371a852
Brie Bencich <bbencich@hotmail.com>
2020-11-02 18:24:07 +0200

    questions added as well as npm dependencie for inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,6 @@
 const fs = require ("fs"); 
 const path = require("path"); 
+const generateMarkdown = require("./utils/generateMarkdown");
 const inquirer = require ("./utils/generateMarkdown"); 
 
 // array of questions for user
@@ -12,18 +13,61 @@ const questions = [
         type: "input", 
         name: "title", 
         message: "What is your project's name?"
-    },{
-        
+    },
+    {
+        type: "input", 
+        name: "email", 
+        message: "What is your email ?"
+    },
+    { 
+        type: "input", 
+        name: "description", 
+        message: "Please write a description of your project"
+    },
+    {
+        type: "list", 
+        name: "liscense", 
+        message: "What kind of license does your project have?", 
+        choices: ["APACHE 2.0", "MIT", "GPL 3.0", "None"]
+    },
+    {
+        type: "iput", 
+        name: "installation", 
+        message: "What command should be run to install the dependencies?", 
+        deafualt: "npm i"
+    }, 
+    {
+        type: "input", 
+        name: "test", 
+        message: "What command should be run to run a test ?", 
+        default: "npm test"
+    },
+    {
+        type: "input", 
+        name: "usage", 
+        message: "Tell the user what they need to know about the repo", 
+    }, 
+    { 
+        type: "input", 
+        name: "usage", 
+        message: "Tell the user what they need to know about contributing to the repo", 
     }
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 // function to initialize program
 function init() {
+    inquirer.prompt(questions)
+    .then((inquirerResponses) => { 
+        console.log("Generating README"); 
+        writeToFile("README.md", generateMarkdown({...inquirerResponses})); 
+
+    })
 
 }
 
